9b45a14c826c865c04272479be342293b74ef752
sandiarbaa <sandiarbaa@gmail.com>
2024-06-16 13:59:01 +0700

    add: hero component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Nav from "./components/Nav";
+import Hero from "./components/Hero";
 
 const HomePage = () => {
   return (
     <div>
       <Nav />
+      <Hero />
     </div>
   );
 };
